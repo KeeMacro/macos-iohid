ad33f40f678c4d279e2abde96dab587e13cb3f36
Michael Ritsema <michaelritsema@gmail.com>
2022-07-20 08:16:02 -0500

    new bridge repo to connect to swift

---
+++ build.rs
@@ -0,0 +1,89 @@
+//
+// Copyright © 2020 Haim Gelfenbeyn
+// This code is licensed under MIT license (see LICENSE.txt for details)
+//
+
+use serde::Deserialize;
+use std::env;
+use std::process::Command;
+
+
+/// This should match whatever is defined in mac_ddc/Package.swift
+/// Anything below 10.15 would require shipping Swift libraries.
+const MACOS_TARGET_VERSION: &str = "12";
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct SwiftTargetInfo {
+    triple: String,
+    unversioned_triple: String,
+    module_triple: String,
+    swift_runtime_compatibility_version: String,
+    #[serde(rename = "librariesRequireRPath")]
+    libraries_require_rpath: bool,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct SwiftPaths {
+    runtime_library_paths: Vec<String>,
+    runtime_library_import_paths: Vec<String>,
+    runtime_resource_path: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct SwiftTarget {
+    target: SwiftTargetInfo,
+    paths: SwiftPaths,
+}
+
+/// Builds mac_ddc library Swift project, sets the library search options right so we link
+/// against Swift run-time correctly.
+fn build_mac_ddc() {
+    let profile = env::var("PROFILE").unwrap();
+    let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let target = format!("{}-apple-macosx{}", arch, MACOS_TARGET_VERSION);
+
+    let swift_target_info_str = Command::new("swift")
+        .args(&["-target", &target, "-print-target-info"])
+        .output()
+        .unwrap()
+        .stdout;
+    let swift_target_info: SwiftTarget = serde_json::from_slice(&swift_target_info_str).unwrap();
+    if swift_target_info.target.libraries_require_rpath {
+        panic!("Libraries require RPath! Change minimum MacOS value to fix.")
+    }
+
+    if !Command::new("swift")
+        .args(&["build", "-c", &profile])
+        .current_dir("../ezmacos/")
+        .status()
+        .unwrap()
+        .success()
+    {
+        panic!("Swift library mac_ddc compilation failed")
+    }
+
+    swift_target_info.paths.runtime_library_paths.iter().for_each(|path| {
+        println!("cargo:rustc-link-search=native={}", path);
+    });
+    println!(
+        "cargo:rustc-link-search=native=../ezmacos/.build/{}/{}",
+        swift_target_info.target.unversioned_triple, profile
+    );
+    println!("cargo:rustc-link-lib=static=ezmacos");
+    println!("cargo:rerun-if-changed=ezmacos/Sources/ezmacos/*.swift");
+    println!("cargo:rustc-env=MACOSX_DEPLOYMENT_TARGET={}", MACOS_TARGET_VERSION)
+}
+
+fn main() {
+    match prost_build::compile_protos(&["../keeproto/keeproto.proto"], &["../keeproto"]) {
+        Ok(msg) => {println!("Proto build:{:?}", msg);},
+        Err(msg) => {panic!("Proto build error:{}", msg);}
+    }
+
+    let target = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    if target == "macos" {
+        build_mac_ddc();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,95 @@
+extern crate prost;
+
+use std::{ptr, alloc::{dealloc, Layout}};
+
+use prost::{Enumeration, Message};
+use bytes::{Bytes, BytesMut, Buf, BufMut};
+
+//pub mod keeproto {
+    include!(concat!(env!("OUT_DIR"), "/keeproto.rs"));
+//}
+
+#[link(name = "ezmacos")]
+extern "C" {
+    //
+    // find if a window is active based on pid
+    // finds pids based on filename
+
+    pub fn is_process_running() -> bool;
+    pub fn list_processes(length: &mut u32, out_bytes: &mut *mut u8);
+}
+
+
+
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+    
+    println!("Hello, world from rust!");
+    unsafe { 
+        //hello();
+    
+       // println!("is_process_running(): {}", is_process_running());
+    }
+    let mut counter = 0;
+    loop { 
+    unsafe { 
+        /* 
+        let kstring = KString{ value:"asdf".to_owned()};
+        let mut kstringlist = KStringList{values:Vec::new()};
+        kstringlist.values.push(kstring);
+        let mut buf = BytesMut::with_capacity(1024);
+        kstringlist.encode(&mut buf);
+
+        let x = KStringList::decode(&mut buf);
+        print!("{:?}", x);
+    */
+        let mut in_length_val = 0;
+        //let in_length =  &mut in_length_val;
+     
+        let mut byte_ptr: *mut u8 = std::ptr::null_mut();
+        let ptr_ptr = &mut  byte_ptr;
+        
+        list_processes(&mut in_length_val, ptr_ptr);
+        // ?? Why  does align want a power of 2 while Swift tells me layou = 1?
+
+        /* 
+        let mut bytes = vec![];
+
+        for i in 0..in_length_val {
+
+           bytes.push( *(*ptr_ptr).offset(i as isize));
+        }
+
+
+        let mut buf = BytesMut::new();
+        buf.put_slice(&bytes[..]);
+ 
+        let mut kstringlist = KStringList{values:Vec::new()};
+
+
+        kstringlist.merge(&mut buf);
+        println!("{:?}", kstringlist);
+     
+        
+       // dealloc appears to work correclty with this alignment
+       // a  memory leak seems to exist around calling a property on
+       //  NSWorkspace.shared.runningApplications
+       // it's a searchable error but people seem to blame XCode
+       */
+       
+       dealloc(byte_ptr, Layout::from_size_align_unchecked((in_length_val) as usize, 1));
+       //println!("Length of byte ptr: {}", in_length_val);
+       counter+=1;
+       if counter % 100 == 0 {
+        print!("counter:{}", counter);
+        println!("Length of byte ptr: {}", in_length_val);
+       }
+
+    }
+    //std::thread::sleep(std::time::Duration::from_millis(1));
+    }   // endloop
+
 }
